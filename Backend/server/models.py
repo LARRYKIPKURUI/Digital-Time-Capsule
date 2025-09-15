d11e7895eb7c21dc950bb1972dc764d6eb387316
LARRY KIPKURUI <larrykipkurui12@gmail.com>
2025-09-15 14:59:34 +0300

    Added password strng length

--- Backend/server/models.py
+++ Backend/server/models.py
@@ -21,7 +21,7 @@ class User(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False, index=True)
     email = db.Column(db.String(120), unique=True, nullable=False, index=True)
-    password_hash = db.Column(db.String(128), nullable=False)
+    password_hash = db.Column(db.String(255), nullable=False)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
 
     capsules = db.relationship('Capsule', backref='user', lazy=True)
