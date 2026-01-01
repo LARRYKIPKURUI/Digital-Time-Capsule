8aa662637d785d1ce429bc3bc88ede6cf236490b
LARRY KIPKURUI <larrykipkurui12@gmail.com>
2026-01-01 23:35:53 +0300

    Re-started Dtabase Instance from server ,pushing changes for backend to pick up

--- Backend/server/start.sh
+++ Backend/server/start.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 # Run database migrations
-echo "Running database migrations..."
+echo "Running database migrations ..."
 flask db upgrade
 
 # Add a delay to give the database time to finish operations
-echo "Migrations done. Waiting 10 seconds for database to catch up..."
+echo "Migrations done. Waiting 10 seconds for database to catch up ..."
 sleep 10
 
 # Start the Flask backend with Gunicorn
